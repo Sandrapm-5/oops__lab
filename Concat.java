f5f0c7b56be2e46a75178fe006e386713992897b
Sandrapm-5 <94552756+Sandrapm-5@users.noreply.github.com>
2022-04-08 12:56:29 +0530

    Add files via upload

---
+++ Concat.java
@@ -0,0 +1,17 @@
+public class Concat{
+	String name;
+	String batch;
+}
+class concat1{	
+	public static void main(String args[]){
+		Concat sa=new Concat();
+		sa.name="anu";
+		sa.batch="mca b";
+		System.out.println(sa.name+""+sa.batch);
+	    
+	}
+		}
+    	
+		
+
+  
