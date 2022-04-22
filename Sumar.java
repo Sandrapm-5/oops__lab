2c839f1e6b6cb4fa367054f9248add86155b27fb
Sandrapm-5 <94552756+Sandrapm-5@users.noreply.github.com>
2022-04-22 11:52:12 +0530

    Add files via upload

---
+++ Sumar.java
@@ -0,0 +1,9 @@
+public class Sumar{
+	public static void main(String args[]){
+		int array[]={1,2,3,4,5};
+	    int sum=array[0]+array[1]+array[2]+array[3]+array[4];
+		System.out.println("sum of array:");
+		System.out.println(sum);
+		
+	}
+}
