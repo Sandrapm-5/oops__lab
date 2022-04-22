84b5bc4d8016797912595db6c3ce63c4e8991ddc
Sandrapm-5 <94552756+Sandrapm-5@users.noreply.github.com>
2022-04-22 12:25:39 +0530

    Add files via upload

---
+++ Copyarray.java
@@ -0,0 +1,12 @@
+public class Copyarray{
+	public static void main(String args[])
+	{      
+	int array1[]={1,2,4,5};
+	int[] array2=array1;
+	System.out.println("copy of array:"+array2);
+	
+	
+	
+	
+	}
+}
