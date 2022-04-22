e246bd52da656ec21979bd5f516f7432833dd704
Sandrapm-5 <94552756+Sandrapm-5@users.noreply.github.com>
2022-04-22 11:53:15 +0530

    Add files via upload

---
+++ Arraysample.java
@@ -0,0 +1,12 @@
+public class Arraysample{
+	public static void main(String args[])
+	{
+		int arr[]={1,2,3,4,5};
+		System.out.println("array elements");
+		System.out.println("1st element"+arr[0]);
+		System.out.println("2nd element"+arr[1]);
+		System.out.println("3rd element"+arr[2]);
+		System.out.println("4th element"+arr[3]);
+		System.out.println("5th element"+arr[4]);
+	}
+}
